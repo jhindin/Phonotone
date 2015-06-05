a6ebf139329b4f5ce756d55bce65930ee2794e93
Joseph Hindin <hindin@gmail.com>
2015-06-05 16:34:10 +0300

    Introducing custom view - continue

--- app/src/main/java/com/jhindin/phonotone/views/KeyView.java
+++ app/src/main/java/com/jhindin/phonotone/views/KeyView.java
@@ -8,19 +8,29 @@ import android.view.View;
 
 public class KeyView extends View {
     Paint mPaint;
+    int mWidth, mHeight;
 
     public KeyView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mPaint = new Paint();
+        mPaint.setColor(0xffffffff);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawLine(0f, 0f, 100f, 100f, mPaint);
-        canvas.drawLine(100f, 0f, 0f, 100f, mPaint);
+        canvas.drawLine(0f, 0f, mWidth - 1, mHeight - 1, mPaint);
+        canvas.drawLine(mWidth - 1, 0f, 0f, mHeight - 1, mPaint);
 
     }
 }
