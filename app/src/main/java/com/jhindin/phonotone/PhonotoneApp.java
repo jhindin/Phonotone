30a25fbc073feda1654d350c83ee13e4a2698216
Joseph Hindin <hindin@gmail.com>
2015-06-05 22:55:03 +0300

    Application class added, jazzhand feature listed in the Manifest

---
+++ app/src/main/java/com/jhindin/phonotone/PhonotoneApp.java
@@ -0,0 +1,10 @@
+package com.jhindin.phonotone;
+
+import android.app.Application;
+
+public class PhonotoneApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
