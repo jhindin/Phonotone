da038e2c456d42f7347146da83364df724ff03ce
Joseph Hindin <hindin@gmail.com>
2015-06-06 10:51:19 +0300

    Listener API changed to include source

--- app/src/main/java/com/jhindin/phonotone/views/ToneListener.java
+++ app/src/main/java/com/jhindin/phonotone/views/ToneListener.java
@@ -1,7 +1,9 @@
 package com.jhindin.phonotone.views;
 
+import android.view.View;
+
 public interface ToneListener {
-    void keyPressed(int musicKey, float pressure);
-    void keyReleased(int musicKey);
-    void pressureChanged(int musicKey, float pressure);
+    void keyPressed(View source, int musicKey, float pressure);
+    void keyReleased(View source, int musicKey);
+    void pressureChanged(View source, int musicKey, float pressure);
 }
